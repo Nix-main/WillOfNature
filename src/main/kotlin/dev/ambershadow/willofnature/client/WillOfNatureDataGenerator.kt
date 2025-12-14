9f25c964c5b889d6b12ce3cc21788e3cd9e94ae9
Nix-main <nicholassnider7@gmail.com>
2025-12-13 19:36:49 -0500

    gay.

---
+++ src/main/kotlin/dev/ambershadow/willofnature/client/WillOfNatureDataGenerator.kt
@@ -0,0 +1,10 @@
+package dev.ambershadow.willofnature.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object WillOfNatureDataGenerator : DataGeneratorEntrypoint {
+    override fun onInitializeDataGenerator(gen: FabricDataGenerator) {
+
+    }
+}
